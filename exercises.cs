5debd54843ba6ff320ffcabf52b49bee882d5129
IN0C3NC10 <bruno.inocencio@fatec.sp.gov.br>
2021-09-19 22:55:26 -0300

    Finished exercise 1 and added exercise 2

--- exercises.cs
+++ exercises.cs
@@ -10,33 +10,48 @@ class main{
         string [] op = new string[2]{"n","0"};
         
         do{
-            
+            Console.Clear();
             Console.WriteLine("Bem-Vindo ao "+ config[0]+" "+config[1]+"..\n");
             menu:
-            Console.WriteLine("Selecione o exercicio que deseja [1 - ] ou [0] para todos os enunciados\n");
-            
+            Console.Write("Selecione o exercicio que deseja [1 - ] ou [0] para todos os enunciados: ");
             op[1]=Console.ReadLine();
+
             if (op[1]=="0")
             {
-                Console.WriteLine("1) Recebi uma quantia e gastei x/x da mesma. Sabendo que me restam R$ x,xx, qual foi a quantidade que recebi?\n");
+                Console.Clear();
+                Console.WriteLine("1) Recebi uma quantia e gastei x/x da mesma. Sabendo que me restam R$ x,xx, qual foi a quantidade que recebi?");
+                Console.WriteLine("2) Estando no dia x de um mês com x dias, pode se dizer que o tempo decorrido é:");
                 goto menu;
             }
             else if (op[1]=="1")
             {
-                float [] fr = new float[3];
+                float [] res = new float[4];
+                Console.Clear();
                 Console.WriteLine("1) Recebi uma quantia e gastei x/x da mesma. Sabendo que me restam R$ x,xx, qual foi a quantidade que recebi?\n");
                 Console.Write("Digite da fração, o numerador [cima/esquerda]: ");
-                fr[0]= float.Parse(Console.ReadLine());
+                res[0]= float.Parse(Console.ReadLine());
                 Console.Write("Digite da fração, o denominador [abaixo/direita]: ");
-                fr[1]= float.Parse(Console.ReadLine());
-                
-                fr[2] = (float) fr[0]/fr[1];
-                Console.WriteLine("\nResultado: "+fr[2]);
+                res[1]= float.Parse(Console.ReadLine());
+                Console.Write("Digite o valor restante: ");
+                res[2]= float.Parse(Console.ReadLine());
+                res[3] = (float) (res[1]*res[2]) / (res[1]-res[0]);
+                Console.WriteLine("\nCom um gasto de "+res[0]+"/"+res[1]+", restando R$"+Math.Round(res[2],2)+", o recebido foi R$"+Math.Round(res[3],2)+".\n");
+            }
+            else if (op[1]=="2")
+            {
+                float [] res = new float[3];
+                Console.Clear();
+                Console.WriteLine("2) Estando no dia x de um mês com x dias, pode se dizer que o tempo decorrido é:\n");
+                Console.Write("Digite o dia atual: ");
+                res[0]= float.Parse(Console.ReadLine());
+                Console.Write("Digite o tamanho do mês: ");
+                res[1]= float.Parse(Console.ReadLine());
+                res[2] = (float) (res[0]/res[1]*100);
+                Console.WriteLine("\nEstando na data de "+res[0]+"/"+res[1]+", o tempo decorrido é de aproximadamente "+Math.Round(res[2],2)+"%.\n");
             }
             
-            Console.WriteLine("Para retornar ao topo digite [S] e para sair digite [N]:");
+            Console.WriteLine("\nPara retornar ao topo digite [S] e para sair digite [N]:");
             op[0]=Console.ReadLine();
-            Console.Clear();
         } while (op[0]=="s" || op[0]=="S");
             
         }
